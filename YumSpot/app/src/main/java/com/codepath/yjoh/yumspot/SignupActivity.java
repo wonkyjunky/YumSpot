8b5581f201c29d7371f37a8be5258f457f0cedf4
wonkyjunky <wjeong@csumb.edu>
2020-04-21 17:56:20 -0700

    SignUp Screen

--- YumSpot/app/src/main/java/com/codepath/yjoh/yumspot/SignupActivity.java
+++ YumSpot/app/src/main/java/com/codepath/yjoh/yumspot/SignupActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
 
     ImageButton btnBack;
+    Button btnSign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +20,23 @@ public class SignupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         btnBack = findViewById(R.id.btnBack);
+        btnSign = findViewById(R.id.btnSign);
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SignupActivity.this, MainActivity.class);
                 startActivity(intent);
+                finish();
+            }
+        });
+
+        btnSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                startActivity(intent);
+                Toast.makeText(SignupActivity.this, "Successfully Signed up", Toast.LENGTH_SHORT).show();
                 finish();
             }
         });
